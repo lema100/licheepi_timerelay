9aef0ecee8595ed7b4f9dc5d81f8e364ab8e5972
Lema <lema_01_@mail.ru>
2020-06-25 16:18:36 -0400

    Signed-off-by: Lema <lema_01_@mail.ru>

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 git submodule init
 git submodule update
-
+cp -a /buildroot_overlay /buildroot
+cd buildroot
+make emb_timerelay_defconfig
+make -j 4
+mkdif ../qt_app/build
+cd ../qt_app/build
+../../buildroot/output/build/qt5base*/bin/qmake ../ CONFIG+=RELEASE
+make -j 4
